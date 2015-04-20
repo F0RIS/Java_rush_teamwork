d77447c397ffd6ffcc745de683ecb03ca018ae5f
F0RIS <f0risw@gmail.com>
2015-04-20 23:46:12 +0400

    GitHub Push test from Windows

---
+++ src/com/javarush/test/level12/lesson12/home07/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.test.level12.lesson12.home07;
+
+/* Fly, Run, Swim для классов Duck, Penguin, Toad
+Есть интерфейсы Fly(летать), Swim(плавать), Run(бегать).
+Добавь эти интерфейсы классам Duck(утка), Penguin(пингвин), Toad(жаба)
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public interface Fly
+    {
+        public void fly();
+    }
+
+    public interface Run
+    {
+        public void run();
+    }
+
+    public interface Swim
+    {
+        public void swim();
+    }
+
+    public class Duck
+    {
+
+    }
+
+    public class Penguin
+    {
+
+    }
+
+    public class Toad
+    {
+
+    }
+}
